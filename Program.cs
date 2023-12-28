49d36f40f91751370fe2d51b0a90c336e35ba75f
jafarkhankishiyev <djafarxankishiev@gmail.com>
2023-12-28 14:45:52 +0400

    Changed addressing the database from id to name + parameterized queries

--- Program.cs
+++ Program.cs
@@ -55,7 +55,12 @@ namespace Library
                     string bookGenre = Console.ReadLine();
                     Console.WriteLine(" \n Введите год выпуска книги:");
                     string bookYear = Console.ReadLine();
-                    await using var command2 = dataSource.CreateCommand($"INSERT INTO books VALUES (\'{bookName}\', \'{bookAuthor}\', \'{bookGenre}\', \'{bookYear}\')");
+
+                    await using var command2 = dataSource.CreateCommand("INSERT INTO books (name, author, genre, released) VALUES (@BookName, @BookAuthor, @BookGenre, @BookYear)");
+                    command2.Parameters.AddWithValue("@BookName", bookName);
+                    command2.Parameters.AddWithValue("@BookAuthor", bookAuthor);
+                    command2.Parameters.AddWithValue("@BookGenre", bookGenre);
+                    command2.Parameters.AddWithValue("@BookYear", bookYear);
                     int number = await command2.ExecuteNonQueryAsync();
                     if (number > 0) {
                         Console.WriteLine($"\n Операция успешно выполнена. {number} объект добавлен.");
@@ -65,7 +70,9 @@ namespace Library
                     await using var command3 = dataSource.CreateCommand("SELECT * FROM books");
                     await using var reader3 = await command3.ExecuteReaderAsync();
                     Console.WriteLine("\n Выберите книгу данные о которой хотите изменить:");
-                                    int i = 1;
+                    int i = 1;
+                    int a = 0;
+                                    
 
                     if (reader3.HasRows) 
                         {
@@ -77,26 +84,46 @@ namespace Library
 
                                 Console.WriteLine($"\n {i}. {name} \t {author} \t {genre} \t {year}");
                                 i++;
+                                a++;
                             }
+                            object[] names = new object[a];
+                            int c = 0;
+                            await using var command4 = dataSource.CreateCommand("SELECT * FROM books");
+                            await using var reader4 = await command4.ExecuteReaderAsync();
+                            while (await reader4.ReadAsync()) {
+                                names[c] = reader4.GetValue(0);
+                                c++;
                             }
-                    Console.WriteLine("\n Введите номер книги:");
-                    string bookChoice = Console.ReadLine();
-                    Console.WriteLine("\n Какие данные хотите изменить? \n 1. Название \n 2. Автор \n 3. Жанр \n 4. Год выпуска \n Введите ответ:");
-                    string dataChoice = Console.ReadLine();
-                    int dataChoiceInt = Int32.Parse(dataChoice);
-                    dataChoiceInt--;
-                    string[] dataChoices = {"name", "author", "genre", "year"};
-                    Console.WriteLine("\n Введите данные, на которые хотите заменить существующие:");
-                    string dataUpdate = Console.ReadLine();
-                    await using var command4 = dataSource.CreateCommand($"UPDATE books SET {dataChoices[dataChoiceInt]}=\'{dataUpdate}\' WHERE id={bookChoice}");
-                    int number = await command4.ExecuteNonQueryAsync();
-                    if (number > 0) {
-                        Console.WriteLine($"\n Операция успешно выполнена. {number} объект обновлен.");
+
+                        
+                            Console.WriteLine("\n Введите номер книги:");
+                            string bookChoice = Console.ReadLine();
+                            int bookChoiceInt = Int32.Parse(bookChoice);
+                            bookChoiceInt--;
+                            Console.WriteLine("\n Какие данные хотите изменить? \n 1. Название \n 2. Автор \n 3. Жанр \n 4. Год выпуска \n Введите номер ответа:");
+                            string dataChoice = Console.ReadLine();
+                            int dataChoiceInt = Int32.Parse(dataChoice);
+                            dataChoiceInt--;
+                            string[] dataChoices = {"name", "author", "genre", "released"};
+                            Console.WriteLine("\n Введите данные, на которые хотите заменить существующие:");
+                            string dataUpdate = Console.ReadLine();
+                            string dataChoicePrepared = dataChoices[dataChoiceInt];
+                            object bookName = names[bookChoiceInt];
+                            string bookNameString = bookName.ToString();
+                            await using var command5 = dataSource.CreateCommand($"UPDATE books SET {dataChoicePrepared}=@DataUpdate WHERE name=@BookName");
+                            command5.Parameters.AddWithValue("@DataUpdate", dataUpdate);
+                            command5.Parameters.AddWithValue("@BookName", bookNameString);
+                            int number = await command5.ExecuteNonQueryAsync();
+                            if (number > 0) {
+                                Console.WriteLine($"\n Операция успешно выполнена. {number} объект обновлен.");
                     }
+                        } 
                 } else if (result == "4") {
                     await using var command5 = dataSource.CreateCommand("SELECT * FROM books");
                     await using var reader5 = await command5.ExecuteReaderAsync();
                     int i = 1;
+                    int a = 0;
+
 
                     if (reader5.HasRows) 
                         {
@@ -108,12 +135,26 @@ namespace Library
 
                                 Console.WriteLine($"\n {i}. {name} \t {author} \t {genre} \t {year}");
                                 i++;
+                                a++;
                             }
+                            object[] names = new object[a];
+                            int c = 0;
+                            await using var command6 = dataSource.CreateCommand("SELECT * FROM books");
+                            await using var reader6 = await command6.ExecuteReaderAsync();
+                            while (await reader6.ReadAsync()) {
+                                names[c] = reader6.GetValue(0);
+                                c++;
                             }
-                    Console.WriteLine("\n Введите номер книги, которую хотите удалить:");
-                    string bookDeleteNum = Console.ReadLine();
-                    await using var command6 = dataSource.CreateCommand($"DELETE FROM books WHERE id={bookDeleteNum};");
-                    await using var reader6 = await command6.ExecuteReaderAsync();
+
+                            
+                            Console.WriteLine("\n Введите номер книги, которую хотите удалить:");
+                            string bookDeleteNum = Console.ReadLine();
+                            int bookDeleteNumInt = Int32.Parse(bookDeleteNum);
+                            bookDeleteNumInt--;
+                            await using var command7 = dataSource.CreateCommand($"DELETE FROM books WHERE name=@BookToDelete;");
+                            command7.Parameters.AddWithValue("@BookToDelete", names[bookDeleteNumInt]);
+                            await using var reader7 = await command7.ExecuteReaderAsync();
+                        }
                 } else {
                     exitNum++;
                 }
